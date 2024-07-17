2660abdb682287a158ce0f5a63fe56f6f7ee2713
igor.kudlo <i.kudla@setplex.com>
2024-07-17 13:51:33 +0300

    feat: filter by name with useDebounce

--- src/components/filters/Filters.tsx
+++ src/components/filters/Filters.tsx
@@ -1,13 +1,29 @@
 import {TextInput, Radio, Group, Select} from '@mantine/core';
 import styles from './styles.module.css';
 import {CharacterGender, CharacterStatus} from "@/stores/characters-type.ts";
+import {useStores} from "@/stores/root-store-context.ts";
+import {useDebounce} from "@/hooks/useDebounce.ts";
+import {ChangeEvent, useEffect, useState} from "react";
 
 function Filters() {
+    const { charactersStore } = useStores();
+    const [name, setName] = useState('');
+    const debouncedValue = useDebounce(name);
+
+    const changeNameHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setName(e.currentTarget.value);
+    }
+
+    useEffect(() => {
+        charactersStore.getAllCharacters({name: debouncedValue})
+    }, [debouncedValue, charactersStore]);
+
     return (
         <div className={styles.filters}>
             <TextInput
                 label="Name"
                 placeholder="Type name"
+                onChange={changeNameHandler}
             />
             <Radio.Group
                 name="characterStatus"
@@ -23,7 +39,7 @@ function Filters() {
             <Select
                 label="Gender"
                 placeholder="Select character gender"
-                data={[CharacterGender.male, CharacterGender.female, CharacterGender.genderless, CharacterGender.unknownGender]}
+                data={['All', CharacterGender.male, CharacterGender.female, CharacterGender.genderless, CharacterGender.unknownGender]}
             />
         </div>
     );
